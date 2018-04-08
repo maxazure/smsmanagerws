da5f68b9633dc9575100871f16add605ad68bcbc
maxazure <maxazure@gmail.com>
2018-04-08 15:18:40 +1200

    unless

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,7 +3,7 @@ RailsAdmin.config do |config|
 config.parent_controller = "ApplicationController"
 
   config.authorize_with do
-      unless !current_user.admin?
+      unless current_user.admin?
         redirect_to(
           main_app.root_path,
           alert: "You are not permitted to view this page"
