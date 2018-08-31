37ead24549719aa84476e6d9b3e5fa540445289f
maxazure <maxazure@gmail.com>
2018-08-31 15:04:38 +1200

    regx

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -31,11 +31,15 @@ class AppointmentController < ApplicationController
       phone_num = msg[1].gsub(/\s+/,'').gsub(/\A64/,'')
       phone_num = "0" + phone_num if phone_num[0,1] != "0"
 
+      next if /\d{6,15}/.match(phone_num).nil?
+
+
       apt = Appointment.new
       apt.fullname = msg[0]
       apt.phone = phone_num
-      apt.appointment_date = msg[3]
-      apt.appointment_when = msg[2]
+
+      apt.appointment_date = msg[3] unless msg[3].nil?
+      apt.appointment_when = msg[2] unless msg[2].nil?
       apt.template_id = current_user.company.templates.first.id
       apt.user_id = current_user.id
       apt.company_id = current_user.company_id
